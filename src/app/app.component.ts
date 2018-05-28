38ce0b2a7f28210caf8b7ab2bb0f5f75f18e55cf
Magali <magali_thuaire@yahoo.fr>
2018-05-29 01:05:37 +0200

    Partie 1 - mise en place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  isAuth = false;
+
+    appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'frigo',
+      status: 'éteint'
+    },
+    {
+      name: 'ordinateur',
+      status: 'allumé'
+    }
+  ]
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
